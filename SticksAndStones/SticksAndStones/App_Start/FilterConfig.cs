a693413208cb7280a9bf67d34b6d2e5ccb23e859
Christopher Lombardi <clombardi@problemsolutions.net>
2014-01-04 10:48:53 -0500

    Started Project

---
+++ SticksAndStones/SticksAndStones/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SticksAndStones
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
