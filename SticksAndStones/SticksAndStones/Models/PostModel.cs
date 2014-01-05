680b5c11d82b3003d8bbcac0d07820a0b80e9c73
Christopher Lombardi <clombardi@problemsolutions.net>
2014-01-04 21:12:06 -0500

    added files

---
+++ SticksAndStones/SticksAndStones/Models/PostModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SticksAndStones.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class PostModel
+    {
+        [Required]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+        [Required]
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+    }
+}
